4c640a1dc8592d9f7c6a0415d7adf047b5d5fd1f
art-abdulwadud <abdulwadudhajike@gmail.com>
2020-08-23 20:56:02 +0300

    add fast forward and fast rewind

--- js/video-player.js
+++ js/video-player.js
@@ -4,8 +4,11 @@ const playerLength = document.querySelector(".player-length");
 const videoInfo = document.querySelector(".video-info");
 const playBtn = document.querySelector("#play");
 const playIcon = document.querySelector("#play-icon");
-const currentTime = document.querySelector('.current-time');
-const duration = document.querySelector('.duration');
+const currentTime = document.querySelector(".current-time");
+const duration = document.querySelector(".duration");
+const fastForward = document.querySelector("#forward");
+const fastRewind = document.querySelector("#rewind");
+const mainPlayBtn = document.querySelector(".main-play-btn");
 
 // Play/Pause video
 const togglePlay = () => {
@@ -24,11 +27,30 @@ const togglePlay = () => {
         "fadeOutRight",
         () => (videoInfo.style.transform = "translateX(100%) translateX(0)")
       );
+  video.paused
+    ? (mainPlayBtn.style.display = "flex")
+    : (mainPlayBtn.style.display = "none");
 };
-// Activate toggle function on click
-videoPlayer.addEventListener("click", () => togglePlay());
 
-video.addEventListener('timeupdate', () => {
-    const videoLenghtPosition = (video.currentTime / video.duration) * 100;
-    playerLength.style.width = videoLenghtPosition + '%';
+// Activate toggle function on click
+video.addEventListener("click", () => togglePlay());
+playBtn.addEventListener("click", () => togglePlay());
+videoInfo.addEventListener("click", () => togglePlay());
+mainPlayBtn.addEventListener("click", () => togglePlay());
+
+video.addEventListener("timeupdate", () => {
+  const videoLenghtPosition = (video.currentTime / video.duration) * 100;
+  playerLength.style.width = videoLenghtPosition + "%";
+  // Show current time and duration
+  const ctRoundedOff = (video.currentTime / 60).toFixed(2);
+  duration.innerHTML = (video.duration / 60).toFixed(2);
+  currentTime.innerHTML = ctRoundedOff;
 });
+
+// Forward or rewind
+const forwardRewind = (secs) => {
+  let time = video.currentTime + secs;
+  video.currentTime = time;
+};
+fastForward.addEventListener("click", () => forwardRewind(5));
+fastRewind.addEventListener("click", () => forwardRewind(-5));
